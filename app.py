dfff0bb03234bf0cbc3db94f5719750017a8487b
mwoo99 <michaelwoo99@yahoo.com>
2017-05-22 00:34:36 -0400

    readd vid path

--- app.py
+++ app.py
@@ -58,7 +58,9 @@ def authenticate():
             return redirect(url_for('home'))#,success="You have logged in"))
         return redirect(url_for('home'))#,error=text))
 
-
+@app.route("/vid/")
+def video():
+    return render_template("minivid.html")
 
 @app.route("/logout/")
 def logout():
